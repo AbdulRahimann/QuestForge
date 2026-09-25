74ba43265a57183caab22cf614a89e0b0647bf68
AbdulRahimann <abdulrahimann631@gmail.com>
2026-09-26 00:30:54 +0530

    add character class and basic battle simulation

--- domain/character.py
+++ domain/character.py
@@ -1,2 +1,18 @@
 class Character:
-    pass
+    def __init__(self, name: str, health: int, attack_power: int):
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
+
+
+    def describe(self) -> str:
+        return f"{self.name} has {self.health} HP and {self.attack_power} ATK"
+
+
+    def attack(self, target: "Character") -> None:
+        target.health -= self.attack_power
+        print(f"{self.name} attacks {target.name} for {self.attack_power} damage!")
+
+    def heal(self, amount: int) -> None:
+        self.health += amount
+        return f"{self.name} heals for {amount} HP!"
