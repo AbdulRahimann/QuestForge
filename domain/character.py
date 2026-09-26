0c031626f6c703ff1e045338e4ba9f1611934310
AbdulRahimann <abdulrahimann631@gmail.com>
2026-09-26 15:54:14 +0530

    encapsulation

--- domain/character.py
+++ domain/character.py
@@ -1,18 +1,29 @@
 class Character:
     def __init__(self, name: str, health: int, attack_power: int):
         self.name = name
-        self.health = health
+        self._health = health
+        self.__max_health = health
         self.attack_power = attack_power
 
+    @property
+    def health(self) -> int:
+        return self._health
 
-    def describe(self) -> str:
-        return f"{self.name} has {self.health} HP and {self.attack_power} ATK"
+    @property
+    def isalive(self) -> bool:
+        return self._health > 0
 
+    def take_damage(self, amount: int ) -> None:
+        if amount < 0:
+            raise ValueError("Damage amount cannot be negative.")
+        self._health  = max(self._health - amount, 0)
 
     def attack(self, target: "Character") -> None:
-        target.health -= self.attack_power
-        print(f"{self.name} attacks {target.name} for {self.attack_power} damage!")
+        if not self.isalive:
+            return
+        target.take_damage(self.attack_power)
+        print(f"{self.name} attacks {target.name} for {self.attack_power} damage.")
 
     def heal(self, amount: int) -> None:
-        self.health += amount
-        return f"{self.name} heals for {amount} HP!"
+        self._health = min(self._health + amount ,self.__max_health)
+        return self._health
--- play.py
+++ play.py
@@ -1,14 +1,17 @@
 import domain.character as character
 
 if __name__ == "__main__":
-    print("QuestForge booting...(level 0 skeleton)")
+    print("QuestForge booting...")
 
-    hero = character.Character("Aria", 100, 15)
+    hero = character.Character("Aria", 50, 15)
     goblin = character.Character("Goblin", 30, 5)
     sensei_moro = character.Character("Sensei Moro", 130, 25)
     wizard = character.Character("Wizard", 120, 20)
+
     
-    
+    # hero.take_damage(50)
+    print(hero.health)
+    print(hero.heal(-25))
 
     # print(hero.describe())
     # print(goblin.describe())
@@ -18,23 +21,25 @@ if __name__ == "__main__":
     # print(goblin.describe())
     # print(goblin.heal(15))
     # print(goblin.describe())
-    print("====== Battle Begins ======:")
-    print(sensei_moro.describe())
-    print(wizard.describe())
-    for i in range(3):
-        print(f"====== Attack_Round {i + 1} ======:")
-        sensei_moro.attack(wizard)
-        print(wizard.describe())
-        wizard.attack(sensei_moro)
-        print(sensei_moro.describe())
-    print("====== Battle Ends ======:")
-    print(wizard.describe())
-    print(sensei_moro.describe())
-    print("====== Healing Begins ======:")
-    print(wizard.heal(30))
-    print(sensei_moro.heal(30))
-    print("======health status after healing ======:")
-    print(wizard.describe())
-    print(sensei_moro.describe())
+
+
+    # print("====== Battle Begins ======:")
+    # print(sensei_moro.describe())
+    # print(wizard.describe())
+    # for i in range(3):
+    #     print(f"====== Attack_Round {i + 1} ======:")
+    #     sensei_moro.attack(wizard)
+    #     print(wizard.describe())
+    #     wizard.attack(sensei_moro)
+    #     print(sensei_moro.describe())
+    # print("====== Battle Ends ======:")
+    # print(wizard.describe())
+    # print(sensei_moro.describe())
+    # print("====== Healing Begins ======:")
+    # print(wizard.heal(30))
+    # print(sensei_moro.heal(30))
+    # print("======health status after healing ======:")
+    # print(wizard.describe())
+    # print(sensei_moro.describe())
 
         
